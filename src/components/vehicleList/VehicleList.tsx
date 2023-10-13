63b7863ae88008f05e53e169c78da735aa01b886
jimmy-cannon <jimmy-cannon@tolw.se>
2023-10-13 15:26:20 +0200

    changed equipments style

--- src/components/vehicleList/VehicleList.tsx
+++ src/components/vehicleList/VehicleList.tsx
@@ -1,8 +1,6 @@
 import { useSelector } from 'react-redux'
 import { RootState } from '../../app/store'
-import { Button, Typography } from '@mui/material'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
+import { Button, Typography, Card, CardContent, Chip, Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 const VehicleList = () => {
@@ -25,37 +23,60 @@ const VehicleList = () => {
 					margin: 'auto',
 					width: '90vw',
 					maxWidth: '900px',
-					textAlign: 'center',
 				}}
 			>
 				{vehicles &&
 					vehicles.map((vehicle, index) => (
 						<Card key={index} style={{ height: '100%' }}>
-							<CardContent style={{ display: 'flex', flexDirection: 'column', height: '90%' }}>
-								<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+							<CardContent
+								style={{
+									display: 'flex',
+									flexDirection: 'column',
+									height: '90%',
+								}}
+							>
+								<Typography
+									style={{ textAlign: 'center', fontWeight: 'bold' }}
+									sx={{ fontSize: 14 }}
+									gutterBottom
+								>
 									{vehicle.id}
 								</Typography>
-								<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-									{vehicle.name}
+								<Typography sx={{ fontSize: 14 }} gutterBottom>
+									{vehicle.name ? vehicle.name : 'No Name'}
 								</Typography>
 								{/* 
 									// Status shouldnt be displayed?
-								<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+								<Typography sx={{ fontSize: 14 }}  gutterBottom>
 									{vehicle.status}
 								</Typography> }
 									// Driver shouldnt be displayed?
-								{ <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+								{ <Typography sx={{ fontSize: 14 }}  gutterBottom>
 									{vehicle.driver}
 								</Typography> */}
-								<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-									{vehicle.fuelType}
+								<Typography sx={{ fontSize: 14 }} gutterBottom>
+									{vehicle.fuelType ? vehicle.fuelType : 'No Fuel Type'}
 								</Typography>
 								{vehicle.equipments?.map((equipment, index) => (
-									<Typography key={index} sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+									<Typography key={index} sx={{ fontSize: 14 }} gutterBottom>
 										{getEquipmentByID(equipment)}
 									</Typography>
 								))}
 								<div style={{ flex: 1 }}></div>
+								{vehicle.equipments ? (
+									<Typography style={{ fontSize: 12, textAlign: 'center', fontWeight: 'bold' }}>
+										Equipments:{' '}
+									</Typography>
+								) : undefined}
+								<Stack direction="row" flexWrap={'wrap'} spacing={1}>
+									{vehicle.equipments?.map((equipment, index) => (
+										<Chip
+											key={index}
+											style={{ margin: '5px' }}
+											label={getEquipmentByID(equipment)}
+										/>
+									))}
+								</Stack>
 								<Button style={{}} component={Link} to={`/vehicles/${vehicle.id}`}>
 									Update Vehicle
 								</Button>
